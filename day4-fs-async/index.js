30f0653141f638036f99ce000fa918792cd71009
Devraj Vaishnav <devrajv2023@gmail.com>
2024-12-12 01:05:50 +0530

    async js code change

--- day4-fs-async/index.js
+++ day4-fs-async/index.js
@@ -1,4 +1,9 @@
 const fs = require("fs");
 
 fs.writeFile("read.txt", "Today is an awesome day", (err) => {
+  if (err) {
+    console.error("Error writing to the file:", err);
+  } else {
+    console.log("File written successfully");
+  }
 });
