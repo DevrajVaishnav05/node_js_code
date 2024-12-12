96b115e476fe2d651a6c8524c102fd5a93506122
Devraj Vaishnav <devrajv2023@gmail.com>
2024-12-13 00:28:22 +0530

    stream code update and new code add

--- day16-streams-modules/index.js
+++ day16-streams-modules/index.js
@@ -1,14 +1,26 @@
 const fs = require("fs");
 const http = require("http");
 
-const server = http.createServer();
 
-server.on("request", (req, res) => {
-  fs.readFile("read.txt", (err, data) => {
-    res.end(data.toString());
-  });
+// server.on("request", (req, res) => {
+//   fs.readFile("read.txt", (err, data) => {
+//     res.end(data.toString());
+//   });
+// });
+const server = http.createServer((req,res)=>{
+
+
+const rstream = fs.createReadStream("read.txt")
+    rstream.on("data",(chunkdata)=>{
+        res.write(chunkdata);
+    });
+    rstream.on("end",()=>{
+        res.end();
+    });
+    rstream.on("error",()=>{
+        res.end("File Not found");
+    });
 });
-
 server.listen(3000, "127.0.0.1", () => {
   console.log("Server is listening on port 3000");
 });
