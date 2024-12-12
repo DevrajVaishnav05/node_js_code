1595f13d6b76565f3581a9470bdd1750621c3f5f
Devraj Vaishnav <devrajv2023@gmail.com>
2024-12-12 14:41:51 +0530

    Import and export module

---
+++ day8-import-export-modules/index.js
@@ -0,0 +1,6 @@
+const {add , name , sub } =  require("./oper.js")
+
+console.log(add(5,5))
+console.log(sub(5,3))
+
+console.log(name)
---
+++ day8-import-export-modules/oper.js
@@ -0,0 +1,8 @@
+const add = (a,b)=>{
+   return  a+b;
+}
+const sub = (a,b)=>{
+    return  a - b;
+ }
+const name = "Devraj vaishnav"
+module.exports ={name, add , sub} ;
