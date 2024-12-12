7d49c5fe154f42d40adf23de97bb8f6404975689
Devraj Vaishnav <devrajv2023@gmail.com>
2024-12-12 15:04:55 +0530

    import npm module

---
+++ day9-import-npm-module/index.js
@@ -0,0 +1,5 @@
+// const chalk = require("chalk");
+const validator = require("validator");
+ const data =  validator.isEmail('dev@dev.com');
+// console.log(chalk.blue('Hello world!'));
+console.log(data);
