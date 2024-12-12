ca4c015e371c491a763e519829db33c70272429e
Devraj Vaishnav <devrajv2023@gmail.com>
2024-12-12 11:39:02 +0530

    async Update and Read Data

--- day4-fs-async/index.js
+++ day4-fs-async/index.js
@@ -1,5 +1,12 @@
 const fs = require("fs");
 
-fs.writeFile("read.txt", "Today is an awesome day", (err) => {
-});
+// fs.writeFile("read.txt", "Today is an awesome day", (err) => {
+// });
 
+fs.appendFile("read.txt","\n Hello Devraj Vaishanv",(err)=>{
+    console.log(err)
+})
+
+fs.readFile("read.txt","utf-8",(err, data) =>{
+    console.log(data)
+})
