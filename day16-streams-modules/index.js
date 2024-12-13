9364d72985aad99ed31a810c634a2606a5d668ee
Devraj Vaishnav <devrajv2023@gmail.com>
2024-12-13 10:49:40 +0530

    Streams modules code error fixed

--- day16-streams-modules/index.js
+++ day16-streams-modules/index.js
@@ -11,15 +11,17 @@ const server = http.createServer((req,res)=>{
 
 
 const rstream = fs.createReadStream("read.txt")
-    rstream.on("data",(chunkdata)=>{
-        res.write(chunkdata);
-    });
-    rstream.on("end",()=>{
-        res.end();
-    });
-    rstream.on("error",()=>{
-        res.end("File Not found");
-    });
+    // rstream.on("data",(chunkdata)=>{
+    //     res.write(chunkdata);
+    // });
+    // rstream.on("end",()=>{
+    //     res.end();
+    // });
+    // rstream.on("error",()=>{
+    //     res.end("File Not found");
+    // });
+
+    rstream.pipe(res)
 });
 server.listen(3000, "127.0.0.1", () => {
   console.log("Server is listening on port 3000");
